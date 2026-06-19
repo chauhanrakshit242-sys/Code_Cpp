7d6b1e48bae683666a4f1d324b27e39a1fe59e56
chauhanrakshit242-sys <chauhanrakshit242@gmail.com>
2026-06-19 12:06:31 +0530

    Rakshit is added

---
+++ Cutrodeinthreepart.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+// solve by Recusion
+// int solve(int n , int x , int y ,int z)
+// {
+// 	if(n==0)
+// 	return 0;
+// 	if(n<0)
+// 	return INT_MIN;
+// 	int a =solve(n-x,x,y,z)+1;
+// 	int b=solve(n-y,x,y,z)+1;
+// 	int c=solve(n-z,x,y,z)+1;
+// 	int ans=max(a,(max(b,c)));
+// 	return ans;
+// }
+// Solve by Memoiaztion
+// int solve(int n , int x , int y ,int z,vector<int>& dp )
+// {
+// 	if(n==0)
+// 	return 0;
+// 	if(n<0)
+// 	return INT_MIN;
+// 	if(dp[n]!=-1)
+// 	{
+// 		return dp[n];
+// 	}
+// 	int a =solve(n-x,x,y,z,dp)+1;
+// 	int b=solve(n-y,x,y,z,dp)+1;
+// 	int c=solve(n-z,x,y,z,dp)+1;
+// 	int ans=max(a,(max(b,c)));
+// 	dp[n]=ans;
+// 	return dp[n];
+// }
+/*Solve by Tabular Form*/
+
+int solve(int n , int x , int y ,int z )
+{
+	
+	vector<int>dp(n+1,INT_MIN);
+    dp[0]=0;
+	for(int i=1;i<=n;i++)
+	{
+		if(i-x>= 0)
+		dp[i]=max(dp[i],dp[i-x]+1);
+		if(i-y>= 0)
+		dp[i]=max(dp[i],dp[i-y]+1);
+		if(i-z>= 0)
+		dp[i]=max(dp[i],dp[i-z]+1);
+	}
+	if(dp[n]<0)
+	return 0;
+	return dp[n];
+}
+int cutSegments(int n, int x, int y, int z) {
+	vector<int>dp(n+1,-1);
+	int ans=solve(n,x,y,z,dp);
+	if(ans<0)
+	{
+		return 0;
+	}
+	return ans;
+}
+int main()
+{
+
+    int n;
+    cin>>n;
+    int x;
+    int y,z;
+    cin>>x>>y>>z;
+    int ans=cutSegments(n,x,y,z); // Solve by recusion only
+    cout<<"Answer:"<<ans<<endl;
+   return 0;
+}
